819a65d35cabf2d739525398bd374084a023a565
quangdu1202 <56354404+quangdu1202@users.noreply.github.com>
2023-12-07 12:47:30 +0700

    Create Factories for Models

---
+++ database/factories/ProductImageFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductImageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ProductImage::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => Product::all()->random()->id,
+            'img_path' => $this->faker->randomElement(['product1.jpg', 'product2.jpg', 'product3.jpg']),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,6 +30,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'verified' => $verified =  fake()->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+            'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+            'admin' => $verified =  fake()->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
         ];
     }
 
